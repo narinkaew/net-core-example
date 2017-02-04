699fee90fe727ecb8705a3913f3d6e077ca35716
Narin Kaewchutima <narin.kaewchutima@gmail.com>
2017-02-05 02:49:31 +0700

    First commit

---
+++ EFCoreScaffoldDbcontext/src/EFCoreScaffoldDbcontext/Data/Entity/BlogExampleContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFCoreScaffoldDbcontext.Data.Entity
+{
+    public partial class BlogExampleContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=.;Database=BlogExample;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blogs>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+
+        public virtual DbSet<Blogs> Blogs { get; set; }
+    }
+}
