ca22733bc59de2a6e6081e0e5ef0c3fb5b4fdd3f
Narin Kaewchutima <narin.kaewchutima@gmail.com>
2017-03-03 12:05:05 +0700

    Using configuration from AppSettings

---
+++ UsingConfigurationFromAppSettings/src/BlogExample/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlogExample.Model;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlogExample.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppSettingsModel _appSettings;
+
+        public BlogController(IOptions<AppSettingsModel> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            string title = _appSettings.Title;
+            string imagePath = _appSettings.ImagePath;
+
+            ViewData["Title"] = title;
+            ViewData["ImagePath"] = imagePath;
+
+            return View();
+        }
+    }
+}
---
+++ UsingConfigurationFromAppSettings/src/BlogExample/Model/AppSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogExample.Model
+{
+    public class AppSettingsModel
+    {
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
