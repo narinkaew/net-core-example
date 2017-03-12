46ea2b405ef78557ea42a53297bfb6667656dcbc
Narin Kaewchutima <narin.kaewchutima@gmail.com>
2017-03-12 11:36:35 +0700

    Building Razor View Using Markdown Syntax

---
+++ BuildingRazorViewUsingMarkdownSyntax/src/BlogExample/Controllers/DocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogExample.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
