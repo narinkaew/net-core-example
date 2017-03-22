aed104853f9256be5e1125e62840c623ad66fc3e
Narin Kaewchutima <narin.kaewchutima@gmail.com>
2017-03-23 01:14:52 +0700

    Convert String To Title Case

---
+++ ConvertStringToTitleCase/src/BlogExample/Controllers/HumanizerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Humanizer;
+
+namespace BlogExample.Controllers
+{
+    public class HumanizerController : Controller
+    {
+        public IActionResult Index()
+        {
+            string TitleCaseFrom = "john doe";
+            ViewData["TitleCaseFrom"] = TitleCaseFrom;
+            ViewData["TitleCaseTo"] = TitleCaseFrom.Transform(To.TitleCase);
+
+            string LowerCaseFrom = "JOHN DOE";
+            ViewData["LowerCaseFrom"] = LowerCaseFrom;
+            ViewData["LowerCaseTo"] = LowerCaseFrom.Transform(To.LowerCase);
+
+            return View();
+        }
+    }
+}
